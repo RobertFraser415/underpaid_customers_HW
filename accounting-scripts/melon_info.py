8de24948f5f9536f1883d7b210e44223daa8f386
Robert Fraser <robert.fraser876@gmail.com>
2022-11-14 17:44:51 -0800

    Complete

---
+++ accounting-scripts/melon_info.py
@@ -0,0 +1,18 @@
+"""Print out all the melons in our inventory."""
+
+
+from melons import melons 
+
+
+def print_melons(melons):
+    """Print each melon with corresponding attribute information."""
+
+    for melon_name, attributes in melons.items():
+        print(melon_name.upper())
+        
+        for attribute, value in attributes.items():
+            print(f"{attribute}: {value}")
+    print('_______________________________________________________')
+
+
+print_melons(melons)
